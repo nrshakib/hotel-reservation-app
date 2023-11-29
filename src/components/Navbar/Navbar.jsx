add67a73a8d693a1aff5f6776ceac5570760fb87
NRS\nrsha <nrshakib007@gmail.com>
2023-11-29 15:21:47 +0600

    Added Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 const Navbar = () => {
   return (
     <nav className="h-14 bg-blue-950 text-white flex justify-center">
-      <div className="w-full max-w-full">
+      <div className="w-full max-w-5xl flex items-center justify-between">
         <span className="font-medium">bookaroom</span>
         <div className="">
-          <button className="border-2 rounded px-2 mr-1">Register</button>
-          <button className="border-2 rounded px-2 ml-1">Log In</button>
+          <button className="border-2 rounded px-2 mr-1 cursor-pointer">
+            Register
+          </button>
+          <button className="border-2 rounded px-2 ml-1 cursor-pointer">
+            Log In
+          </button>
         </div>
       </div>
     </nav>
